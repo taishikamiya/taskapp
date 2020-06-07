c33068afa910ab70a9ee3ef416d0b9155277a31a
Taishi Kamiya <t.kamiya@edu.ciid.dk>
2020-06-07 16:53:17 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by Taishi Kamiya on 2020/06/07.
+//  Copyright © 2020 taishi.kamiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
